5576fa068c2e0a253d6c56dd33bbe93df44ec154
Ahmed Ibrahim Ali <ahmed.ali.ms@alexu.edu.eg>
2023-10-07 23:10:23 +0300

    Adding core basic models for app "product-category-order-orderitem"

---
+++ ZStore.Core/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEcommerce.Domain
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
+
+        [StringLength(150, MinimumLength = 5, ErrorMessage = "Address length must be between 3 to 50 characters")]
+        public string Address { get; set; }
+        public decimal TotalPrice { get; set; }
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public CustomUser User { get; set; }
+
+    }
+}
