4f9b8da95831b993cd2ecddbbcb207a4812ac341
Ahmed Ibrahim Ali <ahmed.ali.ms@alexu.edu.eg>
2023-10-08 10:32:46 +0300

    Adding MCV Tier and applying configuaraion settings

---
+++ ZStore.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZStore.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
